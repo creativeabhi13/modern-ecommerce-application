5179ec8c5cbefe47f13a2ce1b914487e4bf7fa60
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-10-13 17:20:50 +0530

    Basic Setup Done
    Done with starter /basic files and folder.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+   <h1>
+    Hello Welcome To Our Modern Ecommerce Application
+   </h1>
+  );
+}
+
+export default App;
